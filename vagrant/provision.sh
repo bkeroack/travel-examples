a3922a7307105f53da1784d6e245f268d3ad6e88
Benjamen Keroack <bkeroack@gmail.com>
2015-03-11 09:12:46 -0700

    Vagrant: install modern go, fix shell

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+GOVERSION="1.4.2"
+
 # Postgres 9.4 isn't available on Ubuntu trusty yet, so we need to add the PPA
 /vagrant/vagrant/apt.postgresql.org.sh
 
 apt-get dist-upgrade -y
-apt-get install -y httpie curl wget git htop postgresql golang
+apt-get install -y httpie curl wget git htop postgresql
 apt-get autoremove -y
 
 cp /vagrant/vagrant/pg_hba.conf /etc/postgresql/9.4/main/pg_hba.conf
@@ -16,5 +18,12 @@ sleep 2
 sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with password 'postgres';"
 sudo -u postgres psql -U postgres -d postgres -c "create database keyvalue with owner postgres;"
 
+FNAME="go${GOVERSION}.linux-amd64.tar.gz"
+wget "https://storage.googleapis.com/golang/${FNAME}" -O /home/vagrant/${FNAME} -nv
+tar -C /usr/local -xzf "./${FNAME}"
+
+echo 'export PATH="${PATH}:/usr/local/go/bin"' > /home/vagrant/.bash_profile
 echo 'export GOPATH="/home/vagrant/go"' >> /home/vagrant/.bash_profile
+echo 'export PS1="${debian_chroot:+($debian_chroot)}\u@\h:\W\$"' >> /home/vagrant/.bash_profile
+echo 'cd /home/vagrant/go' >> /home/vagrant/.bash_profile 
 chown vagrant /home/vagrant/.bash_profile
