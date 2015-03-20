1932cad5f1d5898939b7ac5a375a0ce18eb56fbd
Benjamen Keroack <bkeroack@gmail.com>
2015-03-20 09:25:04 -0700

    fix vagrant create database commands

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -16,7 +16,8 @@ service postgresql restart
 sleep 2
 
 sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with password 'postgres';"
-sudo -u postgres psql -U postgres -d postgres -c "create database keyvalue with owner postgres; create database keyvalue_testing with owner postgres;"
+sudo -u postgres psql -U postgres -d postgres -c "create database keyvalue with owner postgres;"
+sudo -u postgres psql -U postgres -d postgres -c "create database keyvalue_testing with owner postgres;"
 
 FNAME="go${GOVERSION}.linux-amd64.tar.gz"
 wget "https://storage.googleapis.com/golang/${FNAME}" -O /home/vagrant/${FNAME} -nv
@@ -24,6 +25,6 @@ tar -C /usr/local -xzf "./${FNAME}"
 
 echo 'export PATH="${PATH}:/usr/local/go/bin"' > /home/vagrant/.bash_profile
 echo 'export GOPATH="/home/vagrant/go"' >> /home/vagrant/.bash_profile
-echo 'export PS1="${debian_chroot:+($debian_chroot)}\u@\h:\W\$"' >> /home/vagrant/.bash_profile
+echo 'export PS1="${debian_chroot:+($debian_chroot)}\u@\h:\W\$ "' >> /home/vagrant/.bash_profile
 echo 'cd /home/vagrant/go' >> /home/vagrant/.bash_profile 
 chown vagrant /home/vagrant/.bash_profile
